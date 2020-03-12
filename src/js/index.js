2f7da214d6bf096bafa7dbb8b3e185676e9d556a
AnduMekon <anduabe@yahoo.com>
2020-03-12 12:24:31 -0400

    getInput_done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
-import * as searchViews from './views/searchView';
-import {elements} from './views/base';
+import * as searchView from './views/searchView'
+import {elements} from './views/base'
 /**
  * Global State of the app
  * Search Object
@@ -10,14 +10,16 @@ import {elements} from './views/base';
  */
 const state = { };
 const controlSearch = async () => {
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
+    
     if (query) {
 
         state.search = new Search(query)
 
         await state.search.getResults()
-        state.search.getResults();
-        console.log(state.search.result)
+        
+        
     }
 
 }
