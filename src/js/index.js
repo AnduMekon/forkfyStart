fda3e404674d680880bdc3c108fba68c76d1bdc5
AnduMekon <anduabe@yahoo.com>
2020-03-13 11:45:35 -0400

    commi6

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView'
-import {elements} from './views/base'
+import {elements, renderLoader, clearLoader} from './views/base'
 /**
  * Global State of the app
  * Search Object
@@ -11,14 +11,31 @@ import {elements} from './views/base'
 const state = { };
 const controlSearch = async () => {
     const query = searchView.getInput();
-    console.log(query);
+   
     
     if (query) {
 
         state.search = new Search(query)
 
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
+    
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+
         await state.search.getResults()
         
+        searchView.renderResults(state.search.result);
         
     }
 
